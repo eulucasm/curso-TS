e7b131b7a9531c7a2fa62ddc795cd98cec5d046e
eulucasm <lucaspoletis@gmail.com>
2021-10-24 18:35:10 -0300

    Aula sobre POO - Reforçando a base

---
+++ src/Seção 19 - TypeScript - POO/001-TS-pilares-da-POO/TS-pilares-da-POO.ts
@@ -0,0 +1,20 @@
+/*
+Os Pilares da Programação Orientada a Objetos -> POO
+
+Abstração, Encapsulamento, Herança e o Polimorfismo
+
+Abstração -> Permite isolar de um objeto somente os conceitos que são necessarios para o
+funcionamento do programa;
+No caso, é apenas trazer os itens necessarios para o funcionamento.
+
+Encapsulamento -> Visa Ocultar partes internas de um objeto e exibir apenas o necessario para
+uso externo;
+exemplo do controle, quando precionado um botao de liga/desliga, eu nao preciso saber o que acontece
+por baixo dos panos no baixo nivel da coisa, eu preciso saber apenas que apertando o botão a tv
+vai ligar ou desligar.
+
+Herança -> Visa passar caracteristicas de um objeto para outro.
+
+Polimorfismo ->
+
+*/
