1e21ae5c5e6bccc5431ab0824f2b390df0c5906d
eulucasm <lucaspoletis@gmail.com>
2021-10-27 19:19:59 -0300

    Aula sobre Getters e Setters

---
+++ src/Seção 19 - TypeScript - POO/007-getter-setter/007-getter-setter.ts
@@ -0,0 +1,22 @@
+export class Pessoa {
+  constructor(
+    private nome: string,
+    private sobrenome: string,
+    private idade: number,
+    private _cpf: string,
+  ) {
+    this.cpf = _cpf;
+  }
+
+  set cpf(cpf: string) {
+    this._cpf = cpf;
+  }
+
+  get cpf(): string {
+    return this._cpf.replace(/\D/g, '');
+  }
+}
+
+const pessoa = new Pessoa('Lucas', 'Marques', 31, '123.456.789-99');
+pessoa.cpf = '123.456.789-00';
+console.log(pessoa.cpf);
